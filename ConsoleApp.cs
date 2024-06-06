59d8a91528b8bde795ee36eb22f92251831da913
Anduri Roshan <t-aroshan@microsoft.com>
2024-06-06 10:26:53 +0530

    first commit

--- ConsoleApp.cs
+++ ConsoleApp.cs
@@ -1,1 +1,11 @@
-
+using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
